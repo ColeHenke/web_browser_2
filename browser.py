7425f97512f5ecb9c3c83a20c75715378b349f28
colehenke <colehenke2@gmail.com>
2026-06-09 18:54:14 -0500

    request and response for http

--- browser.py
+++ browser.py
@@ -1,4 +1,6 @@
 import socket
+from urllib import request
+
 
 class Url:
     def __init__(self, url):
@@ -13,3 +15,28 @@ class Url:
     def request(self):
         s = socket.socket(type=socket.AF_INET, family=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
         s.connect((self.host, 80))
+
+        request = 'GET {} HTTP/1.0\r\n'.format(self.path)
+        request += 'Host: {}\r\n'.format(self.host)
+        request += '\r\n'
+        s.send(request.encode())
+
+        response = s.makefile('r', encoding='utf-8', newline='\r\n')
+
+        statuline = response.readline()
+        version, status, message = statuline.split(' ', 2)
+
+        headers = {}
+        while True:
+            line = response.readline()
+            if line == '\r\n':
+                break
+            header, value = line.split(':', 1)
+            headers[header.casefold()] = value.strip()
+
+            assert 'transfer-encoding' not in headers
+            assert 'content-encoding' not in headers
+
+            content = response.read()
+            s.close()
+            return content
