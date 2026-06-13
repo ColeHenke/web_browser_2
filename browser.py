ee0e4fdde76d747e42fcf2d81eead330b415a841
colehenke <colehenke2@gmail.com>
2026-06-13 16:53:09 -0500

    implement downward scroll

--- browser.py
+++ browser.py
@@ -4,6 +4,8 @@ import tkinter
 
 DATA_SCHEME = 'data:text/html,'
 WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
 
 class Url:
     def __init__(self, url):
@@ -77,22 +79,43 @@ class Browser:
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
+        self.display_list = []
+        self.scroll = 0
+
+        # binds
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete('all')
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, url):
-        HSTEP, VSTEP = 13, 18
         cursor_x, cursor_y = HSTEP, VSTEP
 
         body = url.request()
         text = lex(body)
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
+        self.display_list = layout(text)
+        self.draw()
 
-            if cursor_x > WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
 
 
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+
+        if cursor_x > WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
 
 def lex(body):
     text =''
