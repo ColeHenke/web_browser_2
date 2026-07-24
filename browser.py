f2df81b803e087a066d9741334188314bc345838
colehenke <colehenke2@gmail.com>
2026-07-23 22:38:20 -0500

    add support for downloading external stylesheets

--- browser.py
+++ browser.py
@@ -25,6 +25,8 @@ BLOCK_ELEMENTS = [
     "legend", "details", "summary"
 ]
 
+
+
 # global font cache
 FONTS = {}
 
@@ -102,6 +104,21 @@ class Url:
         s.close()
         return content
 
+    def resolve(self, url):
+        if "://" in url: return Url(url)
+        if not url.startswith("/"):
+            dir, _ = self.path.rsplit("/", 1)
+            while url.startswith("../"):
+                _, url = url.split("/", 1)
+                if "/" in dir:
+                    dir, _ = dir.rsplit("/", 1)
+            url = dir + "/" + url
+        if url.startswith("//"):
+            return Url(self.scheme + ":" + url)
+        else:
+            return Url(self.scheme + "://" + self.host + \
+                       ":" + str(self.port) + url)
+
 
 class Browser:
     def __init__(self):
@@ -124,7 +141,23 @@ class Browser:
     def load(self, url):
         body = url.request()
         self.nodes = HtmlParser(body).parse()
-        style(self.nodes)
+        
+        rules = DEFAULT_STYLE_SHEET.copy()
+        links = [node.attributes["href"]
+                 for node in tree_to_list(self.nodes, [])
+                 if isinstance(node, Element)
+                 and node.tag == "link"
+                 and node.attributes.get("rel") == "stylesheet"
+                 and "href" in node.attributes]
+        style(self.nodes, rules)
+        for link in links:
+            style_url = url.resolve(link)
+            try:
+                body = style_url.request()
+            except:
+                continue
+            rules.extend(CSSParser(body).parse())
+
         # print_tree(self.nodes)
         self.document = DocumentLayout(self.nodes)
         self.document.layout()
@@ -471,7 +504,7 @@ class CSSParser:
 
     def body(self):
         pairs = {}
-        while self.i < len(self.s):
+        while self.i < len(self.s) and self.s[self.i] != "}":
             try:
                 prop, val = self.pair()
                 pairs[prop] = val
@@ -479,7 +512,7 @@ class CSSParser:
                 self.literal(";")
                 self.whitespace()
             except Exception:
-                why = self.ignore_until([";"])
+                why = self.ignore_until([';' '}'])
                 if why == ";":
                     self.literal(";")
                     self.whitespace()
@@ -495,17 +528,73 @@ class CSSParser:
                 self.i += 1
         return None
 
+    def selector(self):
+        out = TagSelector(self.word().casefold())
+        self.whitespace()
+        while self.i < len(self.s) and self.s[self.i] != "{":
+            tag = self.word()
+            descendant = TagSelector(tag.casefold())
+            out = DescendantSelector(out, descendant)
+            self.whitespace()
+        return out
 
-def style(node):
+    def parse(self):
+        rules = []
+        while self.i < len(self.s):
+            try:
+                self.whitespace()
+                selector = self.selector()
+                self.literal("{")
+                self.whitespace()
+                body = self.body()
+                self.literal("}")
+                rules.append((selector, body))
+            except Exception:
+                why = self.ignore_until(["}"])
+                if why == "}":
+                    self.literal("}")
+                    self.whitespace()
+                else:
+                    break
+        return rules
+
+
+class TagSelector:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def matches(self, node):
+        return isinstance(node, Element) and self.tag == node.tag
+
+
+class DescendantSelector:
+    def __init__(self, ancestor, descendant):
+        self.ancestor = ancestor
+        self.descendant = descendant
+
+    def matches(self, node):
+        if not self.descendant.matches(node): return False
+        while node.parent:
+            if self.ancestor.matches(node.parent): return True
+            node = node.parent
+        return False
+
+
+def style(node, rules):
     node.style = {}
-    print(node)
+
+    for selector, body in rules:
+        if not selector.matches(node): continue
+        for property, value in body.items():
+            node.style[property] = value
+
     if isinstance(node, Element) and "style" in node.attributes:
         pairs = CSSParser(node.attributes["style"]).body()
         for property, value in pairs.items():
             node.style[property] = value
 
     for child in node.children:
-        style(child)
+        style(child, rules)
 
 
 def paint_tree(layout_object, display_list):
@@ -514,6 +603,13 @@ def paint_tree(layout_object, display_list):
     for child in layout_object.children:
         paint_tree(child, display_list)
 
+def tree_to_list(tree, list):
+    list.append(tree)
+    for child in tree.children:
+        tree_to_list(child, list)
+    return list
+
+DEFAULT_STYLE_SHEET = CSSParser(open("browser.css").read()).parse()
 
 if __name__ == "__main__":
     import sys
